701e7331ae5e5b119d0a07015bfc662add8f0246
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-21 16:36:49 -0500

    Add Homepage to App.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import Navbar from "./src/components/Navbar";
-import SignUp from "./src/components/SignUp";
+import Navbar from './src/components/Navbar';
+// import SignUp from "./src/components/SignUp";
 
-import Button from "./src/components/Button";
+import Button from './src/components/Button';
+import Homepage from './src/components/Homepage';
 
 export default function App() {
   return (
     <SafeAreaView>
       <Navbar />
       <View style={styles.container}>
-        <SignUp />
-
-        <StatusBar style="auto" />
+        {/* <SignUp /> */}
+        <Homepage />
+        <StatusBar style='auto' />
       </View>
     </SafeAreaView>
   );
@@ -22,10 +23,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#f0f0f0",
+    backgroundColor: '#f0f0f0',
     // alignItems: "center",
     // justifyContent: "center",
     // flex: 1,
-    color: "black",
+    color: 'black',
   },
 });
