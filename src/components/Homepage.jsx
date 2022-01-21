76fdeb6a10239a2954508f0cbec2e828a9f90cde
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-21 16:43:29 -0500

    Map over all 6 quick links

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView } from 'react-native';
 
 import HomepageQuickLink from './HomepageQuickLink';
 
 export default function Homepage() {
+  const allQuickLinks = [
+    {title: 'Collaboratory', jpgLocation: require('../../assets/test-square.jpg')},
+    {title: 'Community', jpgLocation: require('../../assets/test-square.jpg')},
+    {title: 'Workshops', jpgLocation: require('../../assets/test-square.jpg')},
+    {title: 'WIN', jpgLocation: require('../../assets/test-square.jpg')},
+    {title: 'ICT Wayfinding', jpgLocation: require('../../assets/test-square.jpg')},
+    {title: 'Resources', jpgLocation: require('../../assets/test-square.jpg')},
+  ]
+
+  const parsedQuickLinks = allQuickLinks.map(element => {
+    return (
+      <HomepageQuickLink
+        key={`quickLinks.${element.title}`}
+        title={element.title}
+        jpgLocation={element.jpgLocation}
+      />
+    );
+  })
+
   return (
-    <HomepageQuickLink
-      title='Collaboratory'
-      jpgLocation={require('../../assets/test-square.jpg')}
-    />
+    <ScrollView>
+      {parsedQuickLinks}
+    </ScrollView>
   );
 }
 
