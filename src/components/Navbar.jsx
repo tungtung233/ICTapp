83df6974ff3ab7f7f2485bb92749d748cf8cb989
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-21 01:03:12 -0500

    Change dropMenu icon to cross when open

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { View, StyleSheet, Image } from "react-native";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import DropMenu from './DropMenu'
+import { Feather } from "@expo/vector-icons";
 
+import DropMenu from "./DropMenu";
 
 export default function Navbar() {
   const [dropMenu, setDropMenu] = useState(false);
@@ -13,22 +14,30 @@ export default function Navbar() {
 
   return (
     <>
-    <View style={styles.navbar}>
-      <Image
-        source={require("../../assets/ICT-Logo.png")}
-        style={styles.ictLogo}
-      />
-      <Ionicons
-        name="md-menu"
-        size={34}
-        color="darkgrey"
-        style={styles.menu}
-        onPress={handleDropMenu}
-      />
-    </View>
-      {dropMenu && 
-        <DropMenu />
-      }
+      <View style={styles.navbar}>
+        <Image
+          source={require("../../assets/ICT-Logo.png")}
+          style={styles.ictLogo}
+        />
+        {!dropMenu ? (
+          <Ionicons
+            name="md-menu"
+            size={34}
+            color="darkgrey"
+            style={styles.menu}
+            onPress={handleDropMenu}
+          />
+        ) : (
+          <Feather
+            name="x"
+            size={25}
+            color="darkgrey"
+            style={styles.menu}
+            onPress={handleDropMenu}
+          />
+        )}
+      </View>
+      {dropMenu && <DropMenu />}
     </>
   );
 }
