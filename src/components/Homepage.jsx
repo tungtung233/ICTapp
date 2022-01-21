e3b3971722f0c52a7e655ccc3f3afe48177a7ed2
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-21 17:03:18 -0500

    Apply styling to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 import HomepageQuickLink from './HomepageQuickLink';
 
+const { height, width } = Dimensions.get('window');
+
 export default function Homepage() {
   const allQuickLinks = [
-    {title: 'Collaboratory', jpgLocation: require('../../assets/test-square.jpg')},
-    {title: 'Community', jpgLocation: require('../../assets/test-square.jpg')},
-    {title: 'Workshops', jpgLocation: require('../../assets/test-square.jpg')},
-    {title: 'WIN', jpgLocation: require('../../assets/test-square.jpg')},
-    {title: 'ICT Wayfinding', jpgLocation: require('../../assets/test-square.jpg')},
-    {title: 'Resources', jpgLocation: require('../../assets/test-square.jpg')},
-  ]
+    {
+      title: 'Collaboratory',
+      jpgLocation: require('../../assets/test-square.jpg'),
+    },
+    {
+      title: 'Community',
+      jpgLocation: require('../../assets/test-square.jpg'),
+    },
+    {
+      title: 'Workshops',
+      jpgLocation: require('../../assets/test-square.jpg'),
+    },
+    { title: 'WIN', jpgLocation: require('../../assets/test-square.jpg') },
+    {
+      title: 'ICT Wayfinding',
+      jpgLocation: require('../../assets/test-square.jpg'),
+    },
+    {
+      title: 'Resources',
+      jpgLocation: require('../../assets/test-square.jpg'),
+    },
+  ];
 
-  const parsedQuickLinks = allQuickLinks.map(element => {
+  const parsedQuickLinks = allQuickLinks.map((element) => {
     return (
       <HomepageQuickLink
         key={`quickLinks.${element.title}`}
@@ -20,13 +38,34 @@ export default function Homepage() {
         jpgLocation={element.jpgLocation}
       />
     );
-  })
+  });
 
   return (
-    <ScrollView>
-      {parsedQuickLinks}
-    </ScrollView>
+    <View style={styles.homepage}>
+      <Text style={styles.text}>{`Lorem ipsum dolor sit amet,`}</Text>
+      <Text style={styles.text}>{`consectetur adipiscing elit.`}</Text>
+      <View style={styles.quickLinks}>{parsedQuickLinks}</View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  homepage: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  text: {
+    width: 2 * (width * 0.45) + 12,
+    marginTop: 5,
+    fontSize: 20,
+  },
+  quickLinks: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'center',
+    height: height - 150,
+  },
+});
