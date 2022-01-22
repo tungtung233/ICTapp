9f7ff6518e11b77c3b055768e13f82a395955b8a
juliasut <juliasutygina@gmail.com>
2022-01-21 22:48:13 -0700

    Add Inter fonts

---
+++ constants/fonts.js
@@ -0,0 +1,6 @@
+const customFonts = {
+  'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
+  'Inter-Medium': require('../assets/fonts/Inter-Medium.ttf'),
+};
+
+export default customFonts;
