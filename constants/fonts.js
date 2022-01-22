5d0f31795da87e8dba72b32127826048ace7a7a8
juliasut <juliasutygina@gmail.com>
2022-01-22 00:34:39 -0700

    Rm files

--- constants/fonts.js
+++
@@ -1,6 +0,0 @@
-const customFonts = {
-  'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
-  'Inter-Medium': require('../assets/fonts/Inter-Medium.ttf'),
-};
-
-export default customFonts;
