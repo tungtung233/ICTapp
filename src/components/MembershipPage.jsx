11278760a9ea6e62b3ecf8c282c096c0f36ca3da
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-21 22:42:51 -0500

    Add Membership image and title

--- src/components/MembershipPage.jsx
+++ src/components/MembershipPage.jsx
@@ -10,11 +10,39 @@ import {
 const { width } = Dimensions.get('window');
 
 export default function MembershipPage() {
-  return <ScrollView style={styles.membershipPage}></ScrollView>;
+  return (
+    <ScrollView style={styles.membershipPage}>
+      <View style={styles.header}>
+        <ImageBackground
+          source={require('../../assets/test-square.jpg')}
+          style={styles.membershipImage}
+        >
+          <Text style={styles.membershipImageTitle}>Membership</Text>
+        </ImageBackground>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   membershipPage: {
     height: '100%',
   },
+  header: {
+    alignItems: 'center',
+  },
+  membershipImage: {
+    margin: 30,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: 190,
+    width: width - 20,
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  membershipImageTitle: {
+    fontSize: 50,
+    color: 'white',
+    marginBottom: 20
+  }
 });
