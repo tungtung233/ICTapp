69896fca8119aad265a4d495292e03981f653084
juliasut <juliasutygina@gmail.com>
2022-01-21 19:15:31 -0700

    Add dummy events data

---
+++ constants/dummy.js
@@ -0,0 +1,42 @@
+export const events = [
+  {
+    id: 1,
+    type: 'Event',
+    title: 'Find Your Best Work',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Non exercitation ullamco reprehenderit incididunt. Officia incididunt id exercitation velit aliqua ut deserunt do non. Aliquip sunt dolor enim occaecat ullamco id consectetur .',
+  },
+  {
+    id: 2,
+    type: 'Workshop',
+    title: 'Resume Workshop',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 3,
+    type: 'Event',
+    title: 'Mastery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 4,
+    type: 'Workshop',
+    title: 'Talent Discovery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+];
