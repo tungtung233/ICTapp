6c8f8a7bd65f8842737df051402ceb4096c748c8
juliasut <juliasutygina@gmail.com>
2022-01-22 14:15:28 -0700

    Make EventsPage scrollable

--- constants/dummy.js
+++ constants/dummy.js
@@ -39,4 +39,74 @@ export const events = [
     description:
       'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
   },
+  {
+    id: 5,
+    type: 'Event',
+    title: 'Find Your Best Work',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Non exercitation ullamco reprehenderit incididunt. Officia incididunt id exercitation velit aliqua ut deserunt do non. Aliquip sunt dolor enim occaecat ullamco id consectetur .',
+  },
+  {
+    id: 6,
+    type: 'Workshop',
+    title: 'Resume Workshop',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 7,
+    type: 'Event',
+    title: 'Mastery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 8,
+    type: 'Workshop',
+    title: 'Talent Discovery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 9,
+    type: 'Workshop',
+    title: 'Resume Workshop',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 10,
+    type: 'Event',
+    title: 'Mastery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
+  {
+    id: 11,
+    type: 'Workshop',
+    title: 'Talent Discovery',
+    date: 'Wed, Jan 19',
+    time: '2:00 pm - 5:00 PM EST',
+    image: require('../assets/test-square.jpg'),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur elit adipiscing elit. Venenatis pulvinar a amet in, suspendisse vitae, posuere eu tortor et. Und commodo, fermentum, mauris leo eget.',
+  },
 ];
