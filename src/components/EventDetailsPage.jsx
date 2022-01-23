2b07848973004d3ccd1c229698e48fdc1c0db03e
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-23 18:53:12 -0500

    Add HeaderBar to EventDetailsPage

--- src/components/EventDetailsPage.jsx
+++ src/components/EventDetailsPage.jsx
@@ -9,6 +9,7 @@ import {
   View,
 } from 'react-native';
 import { Button } from 'react-native-elements';
+import HeaderBar from './HeaderBar';
 
 function handleOpenEventbrite() {
   Linking.openURL(
@@ -18,127 +19,132 @@ function handleOpenEventbrite() {
 
 function EventDetailPage() {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={{ padding: 12 }} />
-        <ImageBackground
-          style={styles.image}
-          source={require('../../assets/EventDetails-Page/event_banner.jpg')}
-        />
-        <View style={{ padding: 23 }} />
+    <>
+      <HeaderBar />
+      <ScrollView>
         <View style={styles.container}>
-          <Text style={styles.titleText}>Find Your Best Work</Text>
-          <View style={styles.twoColumnContainer}>
-            <View>
-              <Text style={styles.date}>WED, JAN 19</Text>
-              <Text style={styles.date}>1:00 PM - 2:00 PM EST</Text>
-              <Text style={styles.price}>$10 / Free for members</Text>
+          <View style={{ padding: 12 }} />
+          <ImageBackground
+            style={styles.image}
+            source={require('../../assets/EventDetails-Page/event_banner.jpg')}
+          />
+          <View style={{ padding: 23 }} />
+          <View style={styles.container}>
+            <Text style={styles.titleText}>Find Your Best Work</Text>
+            <View style={styles.twoColumnContainer}>
+              <View>
+                <Text style={styles.date}>WED, JAN 19</Text>
+                <Text style={styles.date}>1:00 PM - 2:00 PM EST</Text>
+                <Text style={styles.price}>$10 / Free for members</Text>
+              </View>
+              <Button
+                buttonStyle={{
+                  height: 47,
+                  width: 129,
+                  borderRadius: 14,
+                  backgroundColor: '#625B71',
+                }}
+                containerStyle={{ margin: 5 }}
+                title='register now'
+                titleStyle={{ fontSize: 14 }}
+                onPress={handleOpenEventbrite}
+              ></Button>
             </View>
+            <Text style={styles.subtitle}>
+              How to use your strengths to find your best work.
+            </Text>
+            <Text style={styles.description}>
+              When you really understand who you are, you are going to be better
+              at what you do.{' '}
+            </Text>
+            <Text style={styles.description}>
+              Compared with those who do not get to focus on what they do best,
+              people who have the opportunity to use their strengths are 6 times
+              as likely to be engaged in their jobs and more than 3 times as
+              likely to report having an excellent quality of life.
+            </Text>
+            <Text style={styles.description}>
+              {' '}
+              In this highly interactive session, you will learn techniques to
+              identify and embrace your strengths and put them to work for a
+              more engaged life.
+            </Text>
+            <Text style={styles.description}>You will discover:</Text>
+            <View style={styles.container}>
+              <Text style={styles.description}>
+                {''}• The 5 clues of talent that could help you build a language
+                around your ideal work and guide your network to keep you on
+                `top of mind1` for opportunities that align with your greatness
+              </Text>
+              <Text style={styles.description}>
+                {' '}
+                • The difference between a talent and a strength
+              </Text>
+              <Text style={styles.description}>
+                {' '}
+                • Why your weaknesses may not matter as much as you think
+              </Text>
+              <Text style={styles.description}>
+                {' '}
+                • Low-cost tools that can help you build a language around what
+                makes you unique and give you ideas for action
+              </Text>
+            </View>
+            <Text style={styles.description}>
+              {' '}
+              Join us in learning more about identifying and talking about your
+              talents and strengths to attract your best work.
+            </Text>
+
             <Button
               buttonStyle={{
                 height: 47,
                 width: 129,
                 borderRadius: 14,
                 backgroundColor: '#625B71',
+                alignSelf: 'center',
               }}
               containerStyle={{ margin: 5 }}
-              title="register now"
+              title='register now'
               titleStyle={{ fontSize: 14 }}
               onPress={handleOpenEventbrite}
-            ></Button>
-          </View>
-          <Text style={styles.subtitle}>
-            How to use your strengths to find your best work.
-          </Text>
-          <Text style={styles.description}>
-            When you really understand who you are, you are going to be better
-            at what you do.{' '}
-          </Text>
-          <Text style={styles.description}>
-            Compared with those who do not get to focus on what they do best,
-            people who have the opportunity to use their strengths are 6 times
-            as likely to be engaged in their jobs and more than 3 times as
-            likely to report having an excellent quality of life.
-          </Text>
-          <Text style={styles.description}>
-            {' '}
-            In this highly interactive session, you will learn techniques to
-            identify and embrace your strengths and put them to work for a more
-            engaged life.
-          </Text>
-          <Text style={styles.description}>You will discover:</Text>
-          <View style={styles.container}>
-            <Text style={styles.description}>
-              {''}• The 5 clues of talent that could help you build a language
-              around your ideal work and guide your network to keep you on `top
-              of mind1` for opportunities that align with your greatness
-            </Text>
-            <Text style={styles.description}>
-              {' '}
-              • The difference between a talent and a strength
-            </Text>
-            <Text style={styles.description}>
-              {' '}
-              • Why your weaknesses may not matter as much as you think
-            </Text>
-            <Text style={styles.description}>
-              {' '}
-              • Low-cost tools that can help you build a language around what
-              makes you unique and give you ideas for action
-            </Text>
-          </View>
-          <Text style={styles.description}>
-            {' '}
-            Join us in learning more about identifying and talking about your
-            talents and strengths to attract your best work.
-          </Text>
-
-          <Button
-            buttonStyle={{
-              height: 47,
-              width: 129,
-              borderRadius: 14,
-              backgroundColor: '#625B71',
-              alignSelf: 'center',
-            }}
-            containerStyle={{ margin: 5 }}
-            title="register now"
-            titleStyle={{ fontSize: 14 }}
-            onPress={handleOpenEventbrite}
-          />
-          <View style={{ padding: 40 }} />
-          <View style={styles.twoColumnContainer}>
-            <Image
-              style={styles.avatar}
-              source={require('../../assets/EventDetails-Page/event_author.jpg')}
             />
-            <View style={{ height: 169 }}>
-              <Text style={styles.authorName}>{`Anne Marie \nMessier`}</Text>
-              <Text style={styles.authorTitle}>
-                {`Certified Gallup \nStrengths Coach and \nFounder of Straightline \nManagement Solutions`}
+            <View style={{ padding: 40 }} />
+            <View style={styles.twoColumnContainer}>
+              <Image
+                style={styles.avatar}
+                source={require('../../assets/EventDetails-Page/event_author.jpg')}
+              />
+              <View style={{ height: 169 }}>
+                <Text style={styles.authorName}>{`Anne Marie \nMessier`}</Text>
+                <Text style={styles.authorTitle}>
+                  {`Certified Gallup \nStrengths Coach and \nFounder of Straightline \nManagement Solutions`}
+                </Text>
+              </View>
+            </View>
+            <View style={{ padding: 16 }} />
+            <Text style={styles.aboutAuthor}>
+              Ann Marie has helped teams and entrepreneurs in the face of a
+              changing landscape, helping them identify roadblocks to their
+              success and directing their focus to new ways of thinking to
+              create a `career best` experience for all involved. Her unique
+              insights have been gathered and synthesized from her work spanning
+              the corporate boardrooms to the local coffee shops.
+            </Text>
+            <View style={{ alignItems: 'flex-end', marginTop: 15 }}>
+              <Text style={styles.authorContact}>
+                amessier@straightlinegroup.com
+              </Text>
+              <Text style={styles.authorContact}>978-821-8376</Text>
+              <Text style={styles.authorContact}>
+                www.straightlinegroup.com
               </Text>
             </View>
+            <Text>{`\n\n\n`}</Text>
           </View>
-          <View style={{ padding: 16 }} />
-          <Text style={styles.aboutAuthor}>
-            Ann Marie has helped teams and entrepreneurs in the face of a
-            changing landscape, helping them identify roadblocks to their
-            success and directing their focus to new ways of thinking to create
-            a `career best` experience for all involved. Her unique insights
-            have been gathered and synthesized from her work spanning the
-            corporate boardrooms to the local coffee shops.
-          </Text>
-          <View style={{ alignItems: 'flex-end', marginTop: 15 }}>
-            <Text style={styles.authorContact}>
-              amessier@straightlinegroup.com
-            </Text>
-            <Text style={styles.authorContact}>978-821-8376</Text>
-            <Text style={styles.authorContact}>www.straightlinegroup.com</Text>
-          </View>
-          <Text>{`\n\n\n`}</Text>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 }
 
