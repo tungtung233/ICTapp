943adec6b393a3a0af7e48bd965339f9cdd3fb01
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-23 18:44:53 -0500

    Replace ReactNavigationHeader with HeaderBar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
-import React, { useState } from 'react';
-import { View, Image, StatusBar, Text } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
 
 import {
@@ -11,10 +11,6 @@ import {
   Inter_700Bold,
 } from '@expo-google-fonts/inter';
 
-import { Ionicons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-
-import DropMenu from './src/components/DropMenu';
 import EventsPage from './src/components/EventsPage';
 import Homepage from './src/components/Homepage';
 import MembershipPage from './src/components/MembershipPage';
@@ -25,15 +21,7 @@ import PhoneCall from './src/components/PhoneCall';
 import PhoneSMS from './src/components/PhoneSMS';
 
 import { NavigationContainer } from '@react-navigation/native';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
-
-import {
-  createDrawerNavigator,
-  DrawerItemList,
-  DrawerItem,
-} from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerItem } from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -46,49 +34,6 @@ export default function App() {
     Inter_700Bold,
   });
 
-  function LogoTitle() {
-    return (
-      <Image
-        style={{ width: 43, height: 30 }}
-        source={require('./assets/ICT-Logo.png')}
-      />
-    );
-  }
-
-  const [showDropMenu, setShowDropMenu] = useState(false);
-
-  const openDropMenu = () => {
-    setShowDropMenu(true);
-  };
-
-  const closeDropMenu = () => {
-    setShowDropMenu(true);
-  };
-
-  function BurgerMenu(props) {
-    return (
-      <>
-        <View>
-          {!showDropMenu ? (
-            <Ionicons
-              name='md-menu'
-              size={34}
-              color='darkgrey'
-              onPress={openDropMenu}
-            />
-          ) : (
-            <Feather
-              name='x'
-              size={25}
-              color='darkgrey'
-              onPress={closeDropMenu}
-            />
-          )}
-        </View>
-      </>
-    );
-  }
-
   const CustomDrawerContent = (props) => {
     return (
       <>
@@ -96,9 +41,7 @@ export default function App() {
           label='Home'
           onPress={() => props.navigation.navigate('Homepage')}
         />
-        <DrawerItem
-          label='About'
-        />
+        <DrawerItem label='About' />
         <DrawerItem
           label='Membership'
           onPress={() => props.navigation.navigate('Membership')}
@@ -128,19 +71,20 @@ export default function App() {
   return fontsLoaded ? (
     <NavigationContainer>
       <StatusBar style='auto' />
-      {/* {showDropMenu && <DropMenu />} */}
       <Drawer.Navigator
         initialRouteName='Homepage'
-        // screenOptions={{
-        //   headerShadowVisible: false,
-        //   headerStyle: { backgroundColor: '#F6F6F6' },
-        //   headerLeft: (props) => <LogoTitle {...props} />,
-        //   headerTitleStyle: { color: '#F6F6F6' },
-        //   headerRight: (props) => <BurgerMenu {...props} />,
-        // }}
+        screenOptions={{
+          headerShown: false,
+        }}
         drawerContent={(props) => <CustomDrawerContent {...props} />}
       >
-        <Drawer.Screen name='Homepage' component={Homepage} />
+        <Drawer.Screen
+          name='Homepage'
+          component={Homepage}
+          options={{
+            drawerActiveBackgroundColor: 'red',
+          }}
+        />
         <Drawer.Screen name='Workshops' component={EventsPage} />
         <Drawer.Screen name='EventDetailPage' component={EventDetailPage} />
         <Drawer.Screen name='Membership' component={MembershipPage} />
