e491f6c4d9081e80c05217748418c45a4d43cdca
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-22 19:57:30 -0500

    Add navigation to DropMenu

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -1,25 +1,49 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Divider } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 
 import { Ionicons } from '@expo/vector-icons';
 
 export default function DropMenu() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.dropMenu}>
       <Text style={styles.menuItem}>About</Text>
       <Divider width={1} style={styles.divider} />
-      <Text style={styles.menuItem}>Membership</Text>
+      <Text
+        style={styles.menuItem}
+        onPress={() => navigation.navigate('Membership')}
+      >
+        Membership
+      </Text>
       <Divider width={1} style={styles.divider} />
-      <Text style={styles.menuItem}>Contact Us</Text>
+      <Text
+        style={styles.menuItem}
+        onPress={() => navigation.navigate('SignUp')}
+      >
+        Contact Us
+      </Text>
       <Divider width={1} style={styles.divider} />
-      <Text style={styles.menuItem}>Workshops</Text>
+      <Text
+        style={styles.menuItem}
+        onPress={() => navigation.navigate('Workshops')}
+      >
+        Workshops
+      </Text>
       <Divider width={1} style={styles.divider} />
       <Text style={styles.menuItem}>Courses</Text>
       <Divider width={1} style={styles.divider} />
       <Text style={styles.menuItem}>Resources</Text>
       <View style={styles.menuHome}>
-        <Ionicons name='home' size={24} color='black' style={styles.divider} />
+        <Ionicons
+          name='home'
+          size={24}
+          color='black'
+          style={styles.divider}
+          onPress={() => navigation.navigate('Homepage')}
+        />
       </View>
     </View>
   );
