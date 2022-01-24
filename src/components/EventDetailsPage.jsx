5004aa0917c11a03abfe0b893e711aa29d911c79
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-23 20:26:41 -0500

    Update EventDetailsPage to match hifis

--- src/components/EventDetailsPage.jsx
+++ src/components/EventDetailsPage.jsx
@@ -8,12 +8,14 @@ import {
   Text,
   View,
 } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, ListItem } from 'react-native-elements';
+import { Octicons } from '@expo/vector-icons';
+
 import HeaderBar from './HeaderBar';
 
 function handleOpenEventbrite() {
   Linking.openURL(
-    'https://www.eventbrite.com/o/institute-for-career-transitions-16486809428'
+    'https://www.eventbrite.com/e/how-to-use-your-strengths-to-find-your-best-work-tickets-199412547297'
   );
 }
 
@@ -21,14 +23,14 @@ function EventDetailPage() {
   return (
     <>
       <HeaderBar />
-      <ScrollView>
+      <ScrollView style={{ backgroundColor: '#F6F6F6' }}>
         <View style={styles.container}>
           <View style={{ padding: 12 }} />
           <ImageBackground
             style={styles.image}
             source={require('../../assets/EventDetails-Page/event_banner.jpg')}
           />
-          <View style={{ padding: 23 }} />
+          <View style={{ padding: 10 }} />
           <View style={styles.container}>
             <Text style={styles.titleText}>Find Your Best Work</Text>
             <View style={styles.twoColumnContainer}>
@@ -39,23 +41,19 @@ function EventDetailPage() {
               </View>
               <Button
                 buttonStyle={{
-                  height: 47,
-                  width: 129,
-                  borderRadius: 14,
+                  height: 50,
+                  width: 140,
+                  borderRadius: 18,
                   backgroundColor: '#625B71',
                 }}
-                containerStyle={{ margin: 5 }}
                 title='register now'
-                titleStyle={{ fontSize: 14 }}
+                titleStyle={{ fontSize: 15, fontFamily: 'Inter_500Medium' }}
                 onPress={handleOpenEventbrite}
               ></Button>
             </View>
-            <Text style={styles.subtitle}>
-              How to use your strengths to find your best work.
-            </Text>
-            <Text style={styles.description}>
+            <Text style={styles.subheading}>
               When you really understand who you are, you are going to be better
-              at what you do.{' '}
+              at what you do.
             </Text>
             <Text style={styles.description}>
               Compared with those who do not get to focus on what they do best,
@@ -64,58 +62,117 @@ function EventDetailPage() {
               likely to report having an excellent quality of life.
             </Text>
             <Text style={styles.description}>
-              {' '}
               In this highly interactive session, you will learn techniques to
               identify and embrace your strengths and put them to work for a
               more engaged life.
             </Text>
             <Text style={styles.description}>You will discover:</Text>
-            <View style={styles.container}>
-              <Text style={styles.description}>
-                {''}• The 5 clues of talent that could help you build a language
-                around your ideal work and guide your network to keep you on
-                `top of mind1` for opportunities that align with your greatness
-              </Text>
-              <Text style={styles.description}>
-                {' '}
-                • The difference between a talent and a strength
-              </Text>
-              <Text style={styles.description}>
-                {' '}
-                • Why your weaknesses may not matter as much as you think
-              </Text>
-              <Text style={styles.description}>
-                {' '}
-                • Low-cost tools that can help you build a language around what
-                makes you unique and give you ideas for action
-              </Text>
+            <View>
+              <ListItem
+                containerStyle={{
+                  backgroundColor: '#F6F6F6',
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                <Octicons
+                  name='primitive-dot'
+                  size={14}
+                  color='#1B2540'
+                  style={{ paddingBottom: 75 }}
+                />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.description}>
+                    The 5 clues of talent that could help you build a language
+                    around your ideal work and guide your network
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+              <ListItem
+                containerStyle={{
+                  backgroundColor: '#F6F6F6',
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                <Octicons
+                  name='primitive-dot'
+                  size={14}
+                  color='#1B2540'
+                  style={{ paddingBottom: 45 }}
+                />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.description}>
+                    The difference between a talent and a strength
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+              <ListItem
+                containerStyle={{
+                  backgroundColor: '#F6F6F6',
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                <Octicons
+                  name='primitive-dot'
+                  size={14}
+                  color='#1B2540'
+                  style={{ paddingBottom: 45 }}
+                />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.description}>
+                    Why your weaknesses may not matter as much as you think
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+              <ListItem
+                containerStyle={{
+                  backgroundColor: '#F6F6F6',
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                <Octicons
+                  name='primitive-dot'
+                  size={14}
+                  color='#1B2540'
+                  style={{ paddingBottom: 105 }}
+                />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.description}>
+                    Low-cost tools that can help you build a language around
+                    what makes you unique and give you ideas for action
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
             </View>
             <Text style={styles.description}>
-              {' '}
               Join us in learning more about identifying and talking about your
               talents and strengths to attract your best work.
             </Text>
 
             <Button
               buttonStyle={{
-                height: 47,
-                width: 129,
-                borderRadius: 14,
+                height: 60,
+                width: 160,
+                borderRadius: 18,
                 backgroundColor: '#625B71',
                 alignSelf: 'center',
+                marginTop: 15,
+                marginBottom: 30,
               }}
               containerStyle={{ margin: 5 }}
               title='register now'
-              titleStyle={{ fontSize: 14 }}
+              titleStyle={{ fontSize: 17, fontFamily: 'Inter_500Medium' }}
               onPress={handleOpenEventbrite}
             />
-            <View style={{ padding: 40 }} />
-            <View style={styles.twoColumnContainer}>
+            <View style={styles.authorTwoColumnContainer}>
               <Image
                 style={styles.avatar}
                 source={require('../../assets/EventDetails-Page/event_author.jpg')}
               />
-              <View style={{ height: 169 }}>
+              <View style={{ height: 169, justifyContent: 'center' }}>
                 <Text style={styles.authorName}>{`Anne Marie \nMessier`}</Text>
                 <Text style={styles.authorTitle}>
                   {`Certified Gallup \nStrengths Coach and \nFounder of Straightline \nManagement Solutions`}
@@ -140,7 +197,7 @@ function EventDetailPage() {
                 www.straightlinegroup.com
               </Text>
             </View>
-            <Text>{`\n\n\n`}</Text>
+            <Text>{`\n`}</Text>
           </View>
         </View>
       </ScrollView>
@@ -150,36 +207,40 @@ function EventDetailPage() {
 
 const styles = StyleSheet.create({
   avatar: {
-    height: 170,
-    width: 112,
-    borderRadius: 14,
-    backgroundColor: '#988FAD',
-    marginRight: 25,
-    resizeMode: 'contain',
+    height: 200,
+    width: 150,
+    borderRadius: 18,
   },
   aboutAuthor: {
     fontFamily: 'Inter_400Regular',
-    fontSize: 16,
-    lineHeight: 16,
+    fontSize: 20,
+    lineHeight: 30,
     marginBottom: 20,
   },
   authorName: {
-    fontFamily: 'Inter_700Bold',
-    fontSize: 24,
-    lineHeight: 24,
-    marginBottom: 20,
+    fontFamily: 'Inter_400Regular',
+    fontSize: 30,
+    lineHeight: 40,
+    marginBottom: 10,
+    color: '#1B2540',
   },
   authorTitle: {
     fontFamily: 'Inter_500Medium',
     fontSize: 16,
-    lineHeight: 24,
+    lineHeight: 22.5,
     color: '#846352',
   },
   authorContact: {
     fontFamily: 'Inter_400Regular',
-    fontSize: 14,
-    lineHeight: 16,
+    fontSize: 17,
+    lineHeight: 25,
     color: '#846352',
+  },
+  authorTwoColumnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 21,
   },
   container: {
     marginLeft: 11,
@@ -188,30 +249,40 @@ const styles = StyleSheet.create({
   },
   date: {
     fontFamily: 'Inter_400Regular',
-    fontSize: 14,
-    lineHeight: 16,
+    fontSize: 16,
+    lineHeight: 20,
     textTransform: 'uppercase',
     color: '#846352',
     marginRight: 25,
   },
   description: {
     fontFamily: 'Inter_400Regular',
-    fontSize: 14,
-    lineHeight: 16,
+    fontSize: 20,
+    lineHeight: 30,
     marginBottom: 20,
+    color: '#1B2540',
   },
   image: {
-    height: 264,
+    marginTop: 6,
+    height: 315,
     width: '100%',
     borderRadius: 15,
     overflow: 'hidden',
     backgroundColor: '#988FAD',
   },
   price: {
-    fontFamily: 'Inter_500Medium',
-    fontSize: 14,
+    fontFamily: 'Inter_600SemiBold',
+    fontSize: 16,
     lineHeight: 20,
     color: '#846352',
+  },
+  subheading: {
+    color: '#1B2540',
+    fontFamily: 'Inter_400Regular',
+    fontSize: 27,
+    lineHeight: 35,
+    marginTop: 5,
+    marginBottom: 20,
   },
   subtitle: {
     fontFamily: 'Inter_700Bold',
@@ -219,16 +290,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   titleText: {
-    fontFamily: 'Inter_700Bold',
-    fontSize: 28,
-    alignSelf: 'center',
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    lineHeight: 28,
+    fontFamily: 'Inter_400Regular',
+    color: '#1B2540',
+    fontSize: 42,
+    lineHeight: 50,
   },
   twoColumnContainer: {
     flexDirection: 'row',
-    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: 21,
     marginBottom: 21,
   },
